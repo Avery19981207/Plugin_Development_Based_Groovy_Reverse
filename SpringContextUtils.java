ccf74374dc47eb068ca0e9b59ac0e897376a6b61
avery_luo <114273679+Avery19981207@users.noreply.github.com>
2024-06-09 21:31:25 +0800

    Create SpringContextUtils.java

---
+++ SpringContextUtils.java
@@ -0,0 +1,73 @@
+package com.dbappsecurity.zerotrust.iam.resource.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author liuwei
+ * Spring容器上下文
+ */
+@Component
+public class SpringContextUtils implements ApplicationContextAware {
+
+    private static final Logger LOGGER= LoggerFactory.getLogger(SpringContextUtils.class);
+
+    /**
+     *  Spring应用上下文环境
+     */
+    private static ApplicationContext applicationContext;
+
+    /**
+     * 实现ApplicationContextAware接口的回调方法，设置上下文环境
+     */
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        if (SpringContextUtils.applicationContext == null) {
+            SpringContextUtils.applicationContext = applicationContext;
+        }
+        LOGGER.info("========ApplicationContext配置成功========");
+    }
+
+    public static ApplicationContext getApplicationContext() {
+        return applicationContext;
+    }
+
+    /**
+     * 获取对象 这里重写了bean方法，起主要作用
+     * example: getBean("userService")//注意： 类名首字母一定要小写！
+     */
+    public static Object getBean(String beanId) throws BeansException {
+        return applicationContext.getBean(beanId);
+    }
+
+    /**
+     * 通过class获取Bean.
+     *
+     * @param clazz
+     * @param <T>
+     * @return
+     */
+    public static <T> T getBean(Class<T> clazz) {
+        return getApplicationContext().getBean(clazz);
+    }
+
+    /**
+     * 通过name,以及Clazz返回指定的Bean
+     *
+     * @param name
+     * @param clazz
+     * @param <T>
+     * @return
+     */
+    public static <T> T getBean(String name, Class<T> clazz) {
+        return getApplicationContext().getBean(name, clazz);
+    }
+
+
+}
