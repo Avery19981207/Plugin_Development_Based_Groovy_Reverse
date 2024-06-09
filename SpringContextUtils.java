9850ce8f0f621b0eb4a7ef1023342aad173543ab
avery_luo <114273679+Avery19981207@users.noreply.github.com>
2024-06-09 21:33:16 +0800

    Update SpringContextUtils.java

--- SpringContextUtils.java
+++ SpringContextUtils.java
@@ -1,9 +1,5 @@
-package com.dbappsecurity.zerotrust.iam.resource.utils;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
